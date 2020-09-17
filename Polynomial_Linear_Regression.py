60693ba0a7a192202ea4862d9072e7b5143e721d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-17 09:07:28 -0600

    Steps added

--- Polynomial_Linear_Regression.py
+++ Polynomial_Linear_Regression.py
@@ -1,2 +1,16 @@
 # Polynomial Linear Regression tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 17SEP20
+
+# Import the libraries
+
+# Import the dataset
+
+# Train the Linear Regression model on the whole dataset
+
+# Train the Polynomial Regression model on the whole dataset
+
+# Visualize the Linear Regression results
+
+# Visualize the Linear Regression results (for higher resolution and smoother curve)
+
+# Predict a new result with Polynomial Linear Regression
