3c2b2d8a32d413954da52e62ff7ac9a2741c2fc0
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-17 09:21:06 -0600

    Steps added

--- Polynomial_Linear_Regression.py
+++ Polynomial_Linear_Regression.py
@@ -2,8 +2,14 @@
 # Input by Ryan L Buchanan 17SEP20
 
 # Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import the dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:,-1].values
 
 # Train the Linear Regression model on the whole dataset
 
@@ -12,5 +18,7 @@
 # Visualize the Linear Regression results
 
 # Visualize the Linear Regression results (for higher resolution and smoother curve)
+
+# Predict a new result with Linear Regression
 
 # Predict a new result with Polynomial Linear Regression
