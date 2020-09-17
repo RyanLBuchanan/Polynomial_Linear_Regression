c9ef82b0e684cc2ede4fc3541ce5022334cae63b
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-17 10:23:33 -0600

    First LinearRegression model against training data

--- Polynomial_Linear_Regression.py
+++ Polynomial_Linear_Regression.py
@@ -12,10 +12,24 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:,-1].values
 
 # Train the Linear Regression model on the whole dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
 
 # Train the Polynomial Regression model on the whole dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
 
 # Visualize the Linear Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 # Visualize the Linear Regression results (for higher resolution and smoother curve)
 
